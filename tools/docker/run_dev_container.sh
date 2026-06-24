95c44ccf07b12a33e949d1514643050fc62be9cf
zjp <jiping_zhou@foxmail.com>
2026-06-24 22:08:02 +0800

    my: docker compose

--- tools/docker/run_dev_container.sh
+++ tools/docker/run_dev_container.sh
@@ -4,10 +4,16 @@
 
 set -e
 
-SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-ASTER_SRC_DIR=${SCRIPT_DIR}/../..
-CARGO_TOML_PATH=${SCRIPT_DIR}/../../Cargo.toml
-VERSION=$( cat ${ASTER_SRC_DIR}/VERSION )
-IMAGE_NAME="asterinas/asterinas:${VERSION}"
+export SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+export ASTER_SRC_DIR=${SCRIPT_DIR}/../..
+export CARGO_TOML_PATH=${SCRIPT_DIR}/../../Cargo.toml
+export VERSION=$(cat ${ASTER_SRC_DIR}/VERSION)
+export IMAGE_NAME="asterinas/asterinas:${VERSION}"
 
-docker run -it --privileged --network=host -v /dev:/dev -v ${ASTER_SRC_DIR}:/root/asterinas ${IMAGE_NAME}
+#docker run -it --privileged --network=host -v /dev:/dev -v ${ASTER_SRC_DIR}:/root/asterinas ${IMAGE_NAME}
+
+# 2. 使用 docker compose 启动
+docker compose up -d
+
+# 3. “进入”环境
+docker compose exec asterinas fish

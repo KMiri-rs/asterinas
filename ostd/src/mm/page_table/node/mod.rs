daa8e378af79aa675b980e38b4c5eca52279a93c
zjp <jiping_zhou@foxmail.com>
2026-06-28 17:18:26 +0800

    kmiri: dellocate boot pages in dismiss
    close https://github.com/KMiri-rs/KMiri/issues/79

--- ostd/src/mm/page_table/node/mod.rs
+++ ostd/src/mm/page_table/node/mod.rs
@@ -81,7 +81,7 @@ impl<C: PageTableConfig> PageTableNode<C> {
                 page.paddr(),
                 1,
                 crate::arch::PageType::PageTable,
-                8,
+                C::PTE_SIZE,
             );
         }
 

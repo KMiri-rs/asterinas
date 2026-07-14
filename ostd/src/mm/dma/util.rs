6b32c462da63fb944cd54975b9171c2a12b20a07
zjp <jiping_zhou@foxmail.com>
2026-07-14 16:45:08 +0800

    kmiri: don't compile panic_page_convert_error for miri

--- ostd/src/mm/dma/util.rs
+++ ostd/src/mm/dma/util.rs
@@ -252,7 +252,7 @@ unsafe fn dealloc_protect_physical_range(pa_range: &Range<Paddr>) {
     });
 }
 
-#[cfg(all(target_arch = "x86_64", any(debug_assertions, feature = "cvm_guest")))]
+#[cfg(all(target_arch = "x86_64", any(debug_assertions, feature = "cvm_guest"), not(miri)))]
 fn panic_page_convert_error(
     action: &str,
     gpa: Paddr,
